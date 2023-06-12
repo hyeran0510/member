d36aaa6a3e99794422b060724ebf4f179412db5c
hyeran0510 <132330609+hyeran0510@users.noreply.github.com>
2023-06-12 17:56:21 +0900

    컨트롤러 추가중

---
+++ src/main/java/hello/member/Controller/Controller.java
@@ -0,0 +1,48 @@
+package hello.member.Controller;
+
+import org.apache.tomcat.util.net.TLSClientHelloExtractor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data","스프링");
+        return "hello";
+    }
+
+    @GetMapping("mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false ) String name, Model model) {
+        model.addAttribute("name", name);
+        return "template";
+    }
+
+    @GetMapping("string")
+    @ResponseBody
+    public String String(@RequestParam("ran") String name) {
+        return "hello" + name;
+    }
+
+        @GetMapping("api")
+        @ResponseBody
+        public Hello hello(@RequestParam("name") String name) {
+            Hello hello = new Hello();
+            hello.setName(name);
+            return hello;
+        }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
