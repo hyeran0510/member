e8ed21e9d321b3a0bdc4576d31de9d7723448a61
hyeran <hr9151@naver.com>
2023-06-22 23:25:29 +0900

    0

---
+++ src/main/java/hello/member/service/MemberService.java
@@ -0,0 +1,17 @@
+package hello.member.service;
+
+
+import hello.member.domain.Member;
+
+public class MemberService {
+
+    /**
+     * 회원가입
+     */
+
+    public Long join(Member member) {
+
+import hello.member.domain.Member;
+        va
+    }
+}
