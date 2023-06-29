8e6cbc504df237c5249cefdc62ba6cf91b43197a
hyeran0510 <132330609+hyeran0510@users.noreply.github.com>
2023-06-29 17:25:28 +0900

    어렵구요 멤버조회 추가 { } 잘 닫고 잘 열자

--- src/main/java/hello/member/Controller/MemberController.java
+++ src/main/java/hello/member/Controller/MemberController.java
@@ -1,10 +1,15 @@
 package hello.member.Controller;
 
 
+import hello.member.domain.Member;
 import hello.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -13,13 +18,37 @@ public class MemberController {
         @Autowired
         public MemberController(MemberService memberService) {
             this.memberService = memberService;
+        }
 
 
-            @GetMapping(value = "/members/new")
+            @GetMapping(value = "/members/new")   //전달 및 url쳐서 들어가는 건 겟매핑
                     public String createForm() {
                 return "member/createMemberForm";
             }
+
+            @PostMapping("/members/new")  //데이터를 폼에 넣어서 전달
+            public String create(MemberForm form) {
+                Member member = new Member();
+                member.setName(form.getName());
+
+                System.out.println(member.getName()); //웹페이지에서 입력한 값 확인하기
+
+
+                memberService.join(member);
+
+                return "redirect:/";
+            }
+
+                @GetMapping("/members")
+                public String List(Model model){
+                    List<Member> members = memberService.findMembers();
+                    model.addAttribute("members", members);
+                    return "members/memberList";
+
+
+                }
         }
+
     }
 
 
